92570048817cc11c0fe03eea56f5739f8df57905
karthick.marzouqa <karthick@marzouqa.com>
2021-04-03 07:16:55 +0530

    First Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+    product:[
+        {
+            _id:"1",
+            name: "Nike",
+            image: "images/product-1.png",
+            price: 123,
+            brand: "Nike",
+            rating: "4",
+            description:"Test application for product",
+            reviews: "34"
+        },
+        {
+            _id:"2",
+            name: "Adidas",
+            image: "images/product-1.png",
+            price: 123,
+            brand: "Adidas",
+            rating: "4",
+            description:"Test application for product",
+            reviews: "23"
+        },
+        {
+            _id:"3",
+            name: "Adidas",
+            image: "images/product-1.png",
+            price: 123,
+            brand: "Adidas",
+            rating: "4",
+            description:"Test application for product",
+            reviews: "23"
+        },
+        {
+            _id:"4",
+            name: "Adidas",
+            image: "images/product-1.png",
+            price: 123,
+            brand: "Adidas",
+            rating: "4",
+            description:"Test application for product",
+            reviews: "23"
+        },
+        {
+            _id:"5",
+            name: "Adidas",
+            image: "images/product-1.png",
+            price: 123,
+            brand: "Adidas",
+            rating: "4",
+            description:"Test application for product",
+            reviews: "23"
+        }
+    ]
+}
+    
+export default data;
